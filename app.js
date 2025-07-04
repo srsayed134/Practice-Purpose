ad1bd62fa9df97341b921e5134844ffee45edde5
srsayed134 <srsayed134@gmail.com>
2025-07-04 19:44:38 +0600

    Revise JS

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = "Sayed";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+let twitterHandle = "srsayed134"
+console.log(name)
+console.log(whatDoYouWannaBecomeInYourLife)
+console.log(gender)
+console.log(twitterHandle)
