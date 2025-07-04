9c662a6cb50d1c66e6d14a6122849d1ca90e79b6
srsayed134 <srsayed134@gmail.com>
2025-07-04 23:07:14 +0600

    Learned string

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let isJsProgrammingLanguage = true;
-let isJsHard = false;
-let favNumb = 7;
-let add = favNumb + undefined;
-console.log(add)
+let favActorFirstName = "Amir ";
+let favActorLastName = "Khan";
+
+let fullName = favActorFirstName.concat(favActorLastName);
+
+let uppercase = fullName.toUpperCase();
+let message = `My favorite Actor Is ${fullName.toUpperCase()} & say something about your actor`;
+message += ` his best show is silicon valley`;
+
+// console.log(message);
+
+let example = "trimdsd";
+let trimedExample = example.slice(0, 7);
+console.log(trimedExample);
