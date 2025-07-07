add7252060936b74b3f6eb79445118e739ac2f27
srsayed134 <srsayed134@gmail.com>
2025-07-07 21:43:19 +0600

    Update website

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-let favActorFirstName = "Amir ";
-let favActorLastName = "Khan";
+let password = 10;
+if(password = 8){
+    console.log("Password is too short")
+}else if(password >= 8){
+    
+}
 
-let fullName = favActorFirstName.concat(favActorLastName);
-
-let uppercase = fullName.toUpperCase();
-let message = `My favorite Actor Is ${fullName.toUpperCase()} & say something about your actor`;
-message += ` his best show is silicon valley`;
-
-// console.log(message);
-
-let example = "trimdsd";
-let trimedExample = example.slice(0, 7);
-console.log(trimedExample);
