72700a4a735381ffd64051371ef862dfc01b7540
srsayed134 <srsayed134@gmail.com>
2025-07-13 16:59:23 +0600

    Revise JS

---
+++ app.js
@@ -0,0 +1,8 @@
+const  favSingers = ["KK", "Adnan sami", "Mohit cuhan"];
+
+console.log(favSingers[0]);
+const favNumbers = [7, 23, 59, 71];
+
+const mixedArr = ["name", ["others", "array"], 123, false];
+
+console.log(mixedArr[3])
