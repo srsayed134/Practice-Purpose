29e184a07d1498046f420d3661653def18904ab2
srsayed134 <srsayed134@gmail.com>
2025-07-17 10:42:57 +0600

    Revise selector

---
+++ app.js
@@ -0,0 +1,11 @@
+const firstElement = document.querySelector(".first");
+
+console.log(firstElement.innerText);
+
+const secondElement = document.querySelector(".second");
+
+console.log(secondElement.textContent);
+
+const thirdElement = document.querySelector(".third");
+
+console.log(thirdElement.innerHTML);
