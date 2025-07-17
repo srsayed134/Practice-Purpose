714a689db3df6d510e2285e877aafcdc0d776f2e
srsayed134 <srsayed134@gmail.com>
2025-07-17 15:52:28 +0600

    Complete dom style chellange

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-const h1 = document.querySelector("h1")
-console.log(h1.style.color = "teal")
+let container = document.querySelector(".container");
+container.style.height = "400px"; 
+container.style.backgroundColor = "teal"
+
+let h1Color = document.querySelector("h2");
+h1Color.style.color = "skyblue"
+
+let para = document.querySelector(".sub-headings");
+para.style.fontFamily = "sans-serif"
+para.style.color = "white"
+
+let btn = document.querySelector(".btn");
+btn.style.color = "white"
